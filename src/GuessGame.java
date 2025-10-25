5b0cdb261e61c745a2106e19d5201a827d23d66f
Jeyachandran <jeyachandran.work@gmail.com>
2025-10-25 05:41:46 +0530

    game Constructor and main

--- src/GuessGame.java
+++ src/GuessGame.java
@@ -1,6 +1,22 @@
+import java.util.Random;
+
 public class GuessGame {
+    public int getTargetNumber() {
+        return targetNumber;
+    }
+
     private int targetNumber;
     private int guessedNumber;
     private int numberOfAttempts;
+    private Random random = new Random();
+
+    public GuessGame(){
+        targetNumber= random.nextInt(100);
+    }
+
+
+    public void start(){
+
+    }
 
 }
